213a3e4df476621ba70b5cd1deb215e78e6c05b9
Martín Morondo <martinmorondo@gmail.com>
2023-01-16 23:43:08 -0300

    I ended up with the themetoggle component, which is used to change the theme from light to dark

---
+++ src/components/themetoggle/toggle.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
+
+const Toggle = () => {
+
+    const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
+    const themetoggle = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+    
+    return (
+        <div className='nav-ac' onClick={themetoggle}>
+            <WiMoonAltWaningCrescent4 />
+        </div>
+    );
+}
+
+export default Toggle;
