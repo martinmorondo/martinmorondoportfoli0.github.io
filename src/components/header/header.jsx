3fc13f6fdfe7d53ca3bfc9aea4577537287b90f8
Martín Morondo <martinmorondo@gmail.com>
2023-01-16 23:29:43 -0300

    Create a file called contentOption, to add the main information in the components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./style.css";
+import { vscGrabber, VscClose } from 'react-icons/vsc';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Header;
