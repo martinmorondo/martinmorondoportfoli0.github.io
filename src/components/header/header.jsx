05bcfd52173403596ccc6fef01c7eae7e38a2bb3
Martín Morondo <martinmorondo@gmail.com>
2023-01-17 00:16:10 -0300

    Finish coding the header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,71 @@
 import React, { useState } from 'react';
 import "./style.css";
-import { vscGrabber, VscClose } from 'react-icons/vsc';
+import { VscGrabber, VscClose } from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
 import { LOGOTEXT, socialProfiles
  } from '../../contentOption';
+import Toggle from '../themetoggle/toggle';
 
 const Header = () => {
+
+    const [isActive, setActive] = useState('false');
+
+    const handleToggle = () => {
+        setActive(!isActive);
+        document.body.classList.toggle("ovhidden");
+    };
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <header className='fixed-top site-header'>
+                <div className='d-flex align-items-center justify-content-between'>
+                    <Link className='navbar-brand nav-ac' to="/">
+                    </Link>
+                    <div className='d-flex align-items-center'>
+                        <Toggle />
+                        <button className='menu-button nav-ac' onClick={handleToggle}>
+                            {!isActive ? <VscClose /> : <VscGrabber />}
+                        </button>
+                    </div>
+                </div>
+
+                <div className={`site-navigation ${!isActive ? 'menu-opend' : ""}`}>
+                <div className='bg-menu h-100'>
+                    <div className='menu-wrapper'>
+                        <div className='menu-container p-3'>
+                            <ul className='the-menu'>
+                                <li className='menu-item'>
+                                    <Link onClick={handleToggle} to="/" className='my-3'>Home</Link>
+                                </li>
+
+                                <li className='menu-item'>
+                                    <Link onClick={handleToggle} to="/portfolio" className='my-3'>Portfolio</Link>
+                                </li>
+
+                                <li className='menu-item'>
+                                    <Link onClick={handleToggle} to="/about" className='my-3'>About</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='menu-footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3'>
+                    <div className='d-flex'>
+                        <a href={socialProfiles.facebook}>Facebook</a>
+                        <a href={socialProfiles.github}>GitHub</a>
+                        <a href={socialProfiles.twitter}>Twitter</a>
+                    </div>
+                    <p className='copyright m-0'>copyright __ {LOGOTEXT}</p>
+                </div>
+                </div>
+            </header>
+
+            <div className='br-top'></div>
+            <div className='br-bottom'></div>
+            <div className='br-left'></div>
+            <div className='br-right'></div>
+     
+        </>
     );
 }
 
