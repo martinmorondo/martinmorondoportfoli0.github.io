bbf310a9e83d533b26e80444559e9bc10eddc39d
Martín Morondo <martinmorondo@gmail.com>
2023-03-04 23:58:06 -0300

    add two new projects

--- src/components/portfolio/projectImages.js
+++ src/components/portfolio/projectImages.js
@@ -1,21 +1,25 @@
-import project0 from "../../assets/img/movie-app.png";
-import project00 from "../../assets/img/movie-app-2.png";
-import project1 from "../../assets/img/coinbase-screen.png";
-import project11 from "../../assets/img/coinbase-clon.png";
-import project2 from "../../assets/img/web-page (1).png";
-import project22 from "../../assets/img/web-page-22.png";
-import project3 from "../../assets/img/Licuadora.jpeg";
-import project33 from "../../assets/img/licuadoraApagada.png";
-import project4 from "../../assets/img/puzzle-messi.jpeg";
-import project44 from "../../assets/img/Puzzle.jpeg";
-import project5 from "../../assets/img/pokeapi.jpg";
-import project55 from "../../assets/img/pokeapi2.jpg";
-import project6 from "../../assets/img/lista-de-tareas.jpeg";
-import project66 from "../../assets/img/lista-tareas.png";
-import project7 from "../../assets/img/ecommerce.jpg";
-import project77 from "../../assets/img/ecommerce-2.png";
-import project8 from "../../assets/img/weather-app-2.png";
-import project88 from "../../assets/img/weather-app.png";
+import project0 from "../../assets/img/coinbase-screen.png";
+import project00 from "../../assets/img/coinbase-clon.png";
+import project1 from "../../assets/img/quiz-app.png";
+import project11 from "../../assets/img/quiz-app-2.png";
+import project2 from "../../assets/img/whack-a-mole.png";
+import project22 from "../../assets/img/whack-a-mole-2.png";
+import project3 from "../../assets/img/web-page (1).png";
+import project33 from "../../assets/img/web-page-22.png";
+import project4 from "../../assets/img/Licuadora.jpeg";
+import project44 from "../../assets/img/licuadoraApagada.png";
+import project5 from "../../assets/img/puzzle-messi.jpeg";
+import project55 from "../../assets/img/Puzzle.jpeg";
+import project6 from "../../assets/img/movie-app.png";
+import project66 from "../../assets/img/movie-app-2.png";
+import project7 from "../../assets/img/lista-de-tareas.jpeg";
+import project77 from "../../assets/img/lista-tareas.png";
+import project8 from "../../assets/img/pokeapi.jpg";
+import project8 from "../../assets/img/pokeapi2.jpg";
+import project9 from "../../assets/img/ecommerce.jpg";
+import project99 from "../../assets/img/ecommerce-2.png";
+import project10 from "../../assets/img/weather-app-2.png";
+import project1010 from "../../assets/img/weather-app.png";
 
 export {
     project0,
@@ -36,4 +40,8 @@ export {
     project77,
     project8,
     project88,
+    project9,
+    project99,
+    project10,
+    project1010,
 }
