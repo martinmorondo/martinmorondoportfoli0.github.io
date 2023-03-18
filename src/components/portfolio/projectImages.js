4a47872d36d840afa39b0cb8d06c0a93903f8d7d
Martín Morondo <martinmorondo@gmail.com>
2023-03-17 21:51:21 -0300

    modify project names

--- src/components/portfolio/projectImages.js
+++ src/components/portfolio/projectImages.js
@@ -1,56 +1,56 @@
-import project1 from "../../assets/img/quiz-app.png";
-import project11 from "../../assets/img/quiz-app-2.png";
-import project2 from "../../assets/img/whack-a-mole.png";
-import project22 from "../../assets/img/whack-a-mole-2.png";
-import project3 from "../../assets/img/web-page (1).png";
-import project33 from "../../assets/img/web-page-22.png";
-import project4 from "../../assets/img/Licuadora.jpeg";
-import project44 from "../../assets/img/licuadoraApagada.png";
-import project5 from "../../assets/img/puzzle-messi.jpeg";
-import project55 from "../../assets/img/Puzzle.jpeg";
-import project6 from "../../assets/img/movie-app.png";
-import project66 from "../../assets/img/movie-app-2.png";
-import project7 from "../../assets/img/lista-de-tareas.jpeg";
-import project77 from "../../assets/img/lista-tareas.png";
-import project8 from "../../assets/img/pokeapi.jpg";
-import project88 from "../../assets/img/pokeapi2.jpg";
-import project9 from "../../assets/img/ecommerce.jpg";
-import project99 from "../../assets/img/ecommerce-2.png";
-import project10 from "../../assets/img/weather-app-2.png";
-import project1010 from "../../assets/img/weather-app.png";
-import project11 from "../../assets/img/twitter-follow-card.png";
-import project1111 from "../../assets/img/twitter-follow-card-2.png";
-import project12 from "../../assets/img/tic-tac-toe-1.png";
-import project1212 from "../../assets/img/tic-tac-toe-2.png";
-import project13 from "../../assets/img/chatbot-1.png";
-import project1313 from "../../assets/img/chatbot-2.png";
+import project0 from "../../assets/img/quiz-app.png";
+import projectCero from "../../assets/img/quiz-app-2.png";
+import project1 from "../../assets/img/whack-a-mole.png";
+import projectUno from "../../assets/img/whack-a-mole-2.png";
+import project2 from "../../assets/img/web-page (1).png";
+import projectDos from "../../assets/img/web-page-22.png";
+import project3 from "../../assets/img/Licuadora.jpeg";
+import projectTres from "../../assets/img/licuadoraApagada.png";
+import project4 from "../../assets/img/puzzle-messi.jpeg";
+import projectCuatro from "../../assets/img/Puzzle.jpeg";
+import project5 from "../../assets/img/movie-app.png";
+import projectCinco from "../../assets/img/movie-app-2.png";
+import project6 from "../../assets/img/lista-de-tareas.jpeg";
+import projectSeis from "../../assets/img/lista-tareas.png";
+import project7 from "../../assets/img/pokeapi.jpg";
+import projectSiete from "../../assets/img/pokeapi2.jpg";
+import project8 from "../../assets/img/ecommerce.jpg";
+import projectOcho from "../../assets/img/ecommerce-2.png";
+import project9 from "../../assets/img/weather-app-2.png";
+import projectNueve from "../../assets/img/weather-app.png";
+import project10 from "../../assets/img/twitter-follow-card.png";
+import projectDiez from "../../assets/img/twitter-follow-card-2.png";
+import project11 from "../../assets/img/tic-tac-toe-1.png";
+import projectOnce from "../../assets/img/tic-tac-toe-2.png";
+import project12 from "../../assets/img/chatbot-1.png";
+import projectDoce from "../../assets/img/chatbot-2.png";
 
 
 export {
+    project0,
+    projectCero,
     project1,
-    project11,
+    projectUno,
     project2,
-    project22,
+    projectDos,
     project3,
-    project33,
+    projectTres,
     project4,
-    project44,
+    projectCuatro,
     project5,
-    project55,
+    projectCinco,
     project6,
-    project66,
+    projectSeis,
     project7,
-    project77,
+    projectSiete,
     project8,
-    project88,
+    projectOcho,
     project9,
-    project99,
+    projectNueve,
     project10,
-    project1010,
+    projectDiez,
     project11,
-    project1111,
+    projectOnce,
     project12,
-    project1212,
-    project13,
-    project1313,
+    projectDoce,
 }
