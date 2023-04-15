e6f4b547038c178f868cc28bb2915388483c0e02
Martín Morondo <martinmorondo@gmail.com>
2023-04-15 17:52:59 -0300

    Wrapp app in LanguagesProvider for language context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { LanguagesProvider } from './context/LanguagesContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <LanguagesProvider>
     <App />
+    </LanguagesProvider>
   </React.StrictMode>,
 )
