e47f0117a901abae56c1429df3a3b82d7b711679
Martín Morondo <martinmorondo@gmail.com>
2023-05-30 16:23:35 -0300

    change profile image

--- src/contentOption.js
+++ src/contentOption.js
@@ -12,14 +12,9 @@ const introData = {
         second: "I code cool websites 🧑‍💻",
         third: "I develop web pages 💻",
     },
-    description: "I am 23 years old and I am a Front End Developer. I am currently a student of Systems Engineering. I am passionate about technology, learning new skills and providing optimal and efficient solutions to any problem",
-    your_img_url: "https://4kwallpapers.com/images/walls/thumbs_3t/6196.jpg",
+    your_img_url: "https://github.com/martinmorondo/react-portfolio/blob/main/src/assets/img/profile.png?raw=true",
 };
 
-const dataAbout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
-};
 
 const socialProfiles = {
     github: "https://github.com/martinmorondo",
@@ -29,7 +24,6 @@ const socialProfiles = {
 
 export {
     meta,
-    dataAbout,
     introData,
     socialProfiles,
     LOGOTEXT,
