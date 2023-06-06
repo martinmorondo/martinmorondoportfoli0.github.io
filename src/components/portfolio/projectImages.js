87029a95a4230710064a77e210f6967370a38518
Martín Morondo <martinmorondo@gmail.com>
2023-06-05 21:48:15 -0300

    add three projects

--- src/components/portfolio/projectImages.js
+++ src/components/portfolio/projectImages.js
@@ -4,34 +4,36 @@ import project1 from "../../assets/img/whack-a-mole.png";
 import projectOne from "../../assets/img/whack-a-mole-2.png";
 import project2 from "../../assets/img/web-page (1).png";
 import projectTwo from "../../assets/img/web-page-22.png";
-// import project3 from "../../assets/img/Licuadora.jpeg";
-// import projectTres from "../../assets/img/licuadoraApagada.png";
-import project4 from "../../assets/img/puzzle-messi.jpeg";
-import projectFour from "../../assets/img/Puzzle.jpeg";
-import project5 from "../../assets/img/movie-app.png";
-import projectFive from "../../assets/img/movie-app-2.png";
-import project6 from "../../assets/img/lista-de-tareas.jpeg";
-import projectSix from "../../assets/img/lista-tareas.png";
-import project7 from "../../assets/img/pokeapi.jpg";
-import projectSeven from "../../assets/img/pokeapi2.jpg";
-import project8 from "../../assets/img/ecommerce.jpg";
-import projectEight from "../../assets/img/ecommerce-2.png";
-import project9 from "../../assets/img/weather-app-2.png";
-import projectNine from "../../assets/img/weather-app.png";
-import project10 from "../../assets/img/twitter-follow-card.png";
-import projectTen from "../../assets/img/twitter-follow-card-2.png";
-import project11 from "../../assets/img/react-gym.png";
-import projectEleven from "../../assets/img/react-gym-2.png";
-import project12 from "../../assets/img/tic-tac-toe-1.png";
-import projectTwelve from "../../assets/img/tic-tac-toe-2.png";
-import project13 from "../../assets/img/grocery-bud.png";
-import projectThirteen from "../../assets/img/grocery-bud-2.png";
-import project14 from "../../assets/img/react-tour.png";
-import projectFourteen from "../../assets/img/react-tour-2.png";
+import project3 from "../../assets/img/puzzle-messi.jpeg";
+import projectThree from "../../assets/img/Puzzle.jpeg";
+import project4 from "../../assets/img/movie-app.png";
+import projectFour from "../../assets/img/movie-app-2.png";
+import project5 from "../../assets/img/lista-de-tareas.jpeg";
+import projectFive from "../../assets/img/lista-tareas.png";
+import project6 from "../../assets/img/ecommerce.jpg";
+import projectSix from "../../assets/img/ecommerce-2.png";
+import project7 from "../../assets/img/weather-app-2.png";
+import projectSeven from "../../assets/img/weather-app.png";
+import project8 from "../../assets/img/twitter-follow-card.png";
+import projectEight from "../../assets/img/twitter-follow-card-2.png";
+import project9 from "../../assets/img/react-gym.png";
+import projectNine from "../../assets/img/react-gym-2.png";
+import project10 from "../../assets/img/tic-tac-toe-1.png";
+import projectTen from "../../assets/img/tic-tac-toe-2.png";
+import project11 from "../../assets/img/grocery-bud.png";
+import projectEleven from "../../assets/img/grocery-bud-2.png";
+import project12 from "../../assets/img/react-tour.png";
+import projectTwelve from "../../assets/img/react-tour-2.png";
+import project13 from "../../assets/img/preguntados.png";
+import projectThirteen from "../../assets/img/preguntados-2.png";
+import project14 from "../../assets/img/reflex-game.png";
+import projectFourteen from "../../assets/img/reflex-game-2.png";
 import project15 from "../../assets/img/react-shop.png";
 import projectFifteen from "../../assets/img/react-shop-2.png";
-import project16 from "../../assets/img/chatbot-1.png";
-import projectSixteen from "../../assets/img/chatbot-2.png";
+import project16 from "../../assets/img/react-ecommerce.png";
+import projectSixteen from "../../assets/img/react-ecommerce-2.png";
+import project17 from "../../assets/img/chatbot-1.png";
+import projectSeventeen from "../../assets/img/chatbot-2.png";
 
 
 export {
@@ -41,6 +43,8 @@ export {
     projectOne,
     project2,
     projectTwo,
+    project3,
+    projectThree,
     project4,
     projectFour,
     project5,
@@ -67,4 +71,6 @@ export {
     projectFifteen,
     project16,
     projectSixteen,
+    project17,
+    projectSeventeen,
 }
